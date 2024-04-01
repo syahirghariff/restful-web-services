66e431d4df3ee7cc567df2b247f44c11d43e282c
syahirghariff <syahirghariff@gmail.com>
2024-04-01 09:17:05 +0800

    Path varibale

--- src/main/java/com/syahirg/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/syahirg/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.syahirg.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class HelloWorldController {
     @GetMapping("hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id}
+    @GetMapping("hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
