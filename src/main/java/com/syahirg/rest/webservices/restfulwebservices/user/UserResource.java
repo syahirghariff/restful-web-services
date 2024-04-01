a040192042a6b1fe0b80065191ec122b0a2064ec
syahirghariff <syahirghariff@gmail.com>
2024-04-01 18:20:18 +0800

    404 for get request

--- src/main/java/com/syahirg/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/syahirg/rest/webservices/restfulwebservices/user/UserResource.java
@@ -23,7 +23,11 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id: " + id);
+        }
+        return user;
     }
 
     @PostMapping("/users")
