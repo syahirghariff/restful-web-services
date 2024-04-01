47009784ca7df18b6ff507ef06efe6a2812232e1
syahirghariff <syahirghariff@gmail.com>
2024-04-01 18:52:05 +0800

    Implementing validations

--- src/main/java/com/syahirg/rest/webservices/restfulwebservices/exception/CustomExceptionHandler.java
+++ src/main/java/com/syahirg/rest/webservices/restfulwebservices/exception/CustomExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.syahirg.rest.webservices.restfulwebservices.exception;
 
 import com.syahirg.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -26,5 +29,17 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                 ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+        // option1
+        ex.getFieldError().getDefaultMessage();
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
