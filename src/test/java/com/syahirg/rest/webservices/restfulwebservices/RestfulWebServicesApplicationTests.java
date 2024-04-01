24dafe379b2e05a07bd9c8cca22654a5d1f6fec8
syahirghariff <syahirghariff@gmail.com>
2024-04-01 08:10:14 +0800

    first commit

---
+++ src/test/java/com/syahirg/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syahirg.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
