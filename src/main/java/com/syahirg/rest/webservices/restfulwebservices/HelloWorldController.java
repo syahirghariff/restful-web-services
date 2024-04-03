1da5176f90892b8dfe546f10a0a1f2868efe8c66
syahirghariff <syahirghariff@gmail.com>
2024-04-03 09:33:12 +0800

    i18n configuration

--- src/main/java/com/syahirg/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/syahirg/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.syahirg.rest.webservices.restfulwebservices;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path="/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path="/hello-world-i18n")
+    public String helloWorldI18n(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 
     @GetMapping("hello-world-bean")
