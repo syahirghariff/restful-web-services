ee17ac35720605ed0aaa06508e41d126b8a347d3
syahirghariff <syahirghariff@gmail.com>
2024-04-06 14:27:15 +0800

    H2 DB. Entity. Automatic execute data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id, birth_date, name)
+values(10001, current_date(), 'Arish test');
+
+insert into user_details(id, birth_date, name)
+values(10002, current_date(), 'Syahir test');
+
+insert into user_details(id, birth_date, name)
+values(10003, current_date(), 'Ghariff test');
